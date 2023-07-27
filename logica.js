66aa09b0d66849aad672f3693238ded758d9295b
Franco Fadail <franco.fada@yahoo.com.ar>
2023-07-27 18:31:12 -0300

    1.1

--- logica.js
+++ logica.js
@@ -1,5 +1,6 @@
-let productos;
+
 jsonProds()
+let productos;
 //OBTENCION DE JSON
 async function jsonProds(){
     const URLJSON = 'productos.json';
@@ -7,6 +8,110 @@ async function jsonProds(){
     const datos = await respuesta.json();
     productos = datos;
     listaInicio(productos);
+
+    
+    
+// OBTENCION DE PRECIOS MAXIMOS
+// OBTENCION DE BOTONES
+const buscarBtn = document.getElementById('buscarBtn');
+const precioMaximoInput = document.getElementById('precioMaximo');
+
+//FILTRADO DE PRECIOS
+function precioFiltrado(precio){
+    const filtrados = productos.filter((prod)=>prod.precio <= precio);
+    return filtrados;
+}
+//OBTENCION DE PRECIOS MAXIMO
+const precios = productos.map(producto => producto.precio);
+const precioMax = Math.max(...precios);
+console.log("Producto maximo: "+precioMax);
+//FUNCION BUSCAR PRECIO
+buscarBtn.addEventListener('click', function() {
+// Obtiene el valor del campo de entrada de texto
+
+ const precioMaximoLocal = parseFloat(precioMaximoInput.value);
+
+//VERIFICAR LOS PRECIOS CORRESPONDIENTES
+  if (isNaN(precioMaximoLocal)|| precioMaximoLocal<0 ||precioMaximoLocal>precioMax) {
+    Swal.fire({
+        icon: 'error',
+        title: 'PRECIO MAXIMO $249000' ,
+        text: 'INGRESE NUEVAMENTE EL MONTO',
+      })
+    }
+    else {
+        const productosMaximos = precioFiltrado(precioMaximoLocal);
+        console.log("Producto maximo: "+productosMaximos);
+        listaInicio(productosMaximos);
+    }
+  }
+);
+//FUNCION REINICIAR (BUSCAR PRECIO)
+const borrarBtn = document.getElementById('borrarBtn');
+function reiniciarFuncion(){
+    precioMaximoInput.value = '';
+    listaInicio(productos);
+}
+borrarBtn.addEventListener('click', reiniciarFuncion);
+
+    //ACCESO A FILTROS - MAYOR A MENOR - MAXIMO PRECIO
+let btnOpciones = document.getElementById('opciones');
+//FUNCION OPCION MENOR A MAYOR (PRECIOS)
+function ordenarMenorAMayor(){
+    productos.sort((a,b)=> a.precio - b.precio);
+    console.table(productos);
+}
+function ordenarMayorAMenor(){
+    productos.sort((a,b)=> b.precio - a.precio);
+    console.table(productos);
+}
+function ordenarAlfabeta(){
+    productos.sort((a, b) => {
+        if(a.titulo < b.titulo) { return -1; }
+        return 0;
+    });
+    console.table(productos);
+}
+
+//ACCIONAR FUNCION DE OPCCION
+btnOpciones.addEventListener("change", accionarOpcion);
+function accionarOpcion(){
+    console.log("Evento AccionarOpcion disparado");
+    const opcionSeleccionada = btnOpciones.value;
+
+    if (opcionSeleccionada === "opcion2") {
+        ordenarMenorAMayor();
+        listaInicio(productos);
+    } 
+    else if(opcionSeleccionada === "opcion3"){
+        ordenarMayorAMenor();
+        listaInicio(productos);
+    }
+    else if(opcionSeleccionada === "opcion1"){
+        ordenarAlfabeta()
+        listaInicio(productos);
+        
+    }
+
+}
+
+//FUNCION BOTON COMPRAR (AGREGA AL CARRO) 
+function compraBoton(){
+    let btnCompra = document.getElementsByClassName('compra');
+        for(const boton of btnCompra){
+            boton.addEventListener('click',()=>{
+                const prodACarro = productos.find((producto)=>producto.id == boton.id);
+                agregarCarrito(prodACarro);
+            })
+            
+            boton.onmouseover = ()=>{
+                boton.classList.replace('btn-outline-warning','btn-warning');
+            }
+            boton.onmouseout = ()=>{
+                boton.classList.replace('btn-warning','btn-outline-warning');
+            }
+        }
+}
 }
 //CONTENIDO DE PRODUCTOS
 let contenidoProductos = document.getElementById('cartasProd');
@@ -134,105 +239,3 @@ btnGift.addEventListener("click",function(){
     })
 })
 //MENU-----END
-
-// OBTENCION DE PRECIOS MAXIMOS
-// OBTENCION DE BOTONES
-const buscarBtn = document.getElementById('buscarBtn');
-const precioMaximoInput = document.getElementById('precioMaximo');
-
-//FILTRADO DE PRECIOS
-function precioFiltrado(precio){
-    const filtrados = productos.filter((prod)=>prod.precio <= precio);
-    return filtrados;
-}
-//OBTENCION DE PRECIOS MAXIMO
-const precios = productos.map(producto => producto.precio);
-const precioMax = Math.max(...precios);
-console.log("Producto maximo: "+precioMax);
-//FUNCION BUSCAR PRECIO
-buscarBtn.addEventListener('click', function() {
-// Obtiene el valor del campo de entrada de texto
-
- const precioMaximoLocal = parseFloat(precioMaximoInput.value);
-
-//VERIFICAR LOS PRECIOS CORRESPONDIENTES
-  if (isNaN(precioMaximoLocal)|| precioMaximoLocal<0 ||precioMaximoLocal>precioMax) {
-    Swal.fire({
-        icon: 'error',
-        title: 'PRECIO MAXIMO $249000' ,
-        text: 'INGRESE NUEVAMENTE EL MONTO',
-      })
-    }
-    else {
-        const productosMaximos = precioFiltrado(precioMaximoLocal);
-        console.log("Producto maximo: "+productosMaximos);
-        listaInicio(productosMaximos);
-    }
-  }
-);
-//FUNCION REINICIAR (BUSCAR PRECIO)
-const borrarBtn = document.getElementById('borrarBtn');
-function reiniciarFuncion(){
-    precioMaximoInput.value = '';
-    listaInicio(productos);
-}
-borrarBtn.addEventListener('click', reiniciarFuncion);
-
-//ACCESO A FILTROS - MAYOR A MENOR - MAXIMO PRECIO
-let btnOpciones = document.getElementById('opciones');
-//FUNCION OPCION MENOR A MAYOR (PRECIOS)
-function ordenarMenorAMayor(){
-    productos.sort((a,b)=> a.precio - b.precio);
-    console.table(productos);
-}
-function ordenarMayorAMenor(){
-    productos.sort((a,b)=> b.precio - a.precio);
-    console.table(productos);
-}
-function ordenarAlfabeta(){
-    productos.sort((a, b) => {
-        if(a.titulo < b.titulo) { return -1; }
-        return 0;
-    });
-    console.table(productos);
-}
-
-//ACCIONAR FUNCION DE OPCCION
-btnOpciones.addEventListener("change", accionarOpcion);
-function accionarOpcion(){
-    console.log("Evento AccionarOpcion disparado");
-    const opcionSeleccionada = btnOpciones.value;
-
-    if (opcionSeleccionada === "opcion2") {
-        ordenarMenorAMayor();
-        listaInicio(productos);
-    } 
-    else if(opcionSeleccionada === "opcion3"){
-        ordenarMayorAMenor();
-        listaInicio(productos);
-    }
-    else if(opcionSeleccionada === "opcion1"){
-        ordenarAlfabeta()
-        listaInicio(productos);
-        
-    }
-
-}
-
-//FUNCION BOTON COMPRAR (AGREGA AL CARRO) 
-function compraBoton(){
-    let btnCompra = document.getElementsByClassName('compra');
-        for(const boton of btnCompra){
-            boton.addEventListener('click',()=>{
-                const prodACarro = productos.find((producto)=>producto.id == boton.id);
-                agregarCarrito(prodACarro);
-            })
-            
-            boton.onmouseover = ()=>{
-                boton.classList.replace('btn-outline-warning','btn-warning');
-            }
-            boton.onmouseout = ()=>{
-                boton.classList.replace('btn-warning','btn-outline-warning');
-            }
-        }
-}
